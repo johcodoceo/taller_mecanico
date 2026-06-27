016d00df5bc68a322366c938d4746e0e6c3bd463
johcodoceo <joh.codoceo@duocuc.cl>
2026-06-27 01:36:57 -0400

    Delete auth-service/auth-service directory

--- auth-service/auth-service/src/main/java/com/taller_mecanico/auth_service/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.taller_mecanico.auth_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
